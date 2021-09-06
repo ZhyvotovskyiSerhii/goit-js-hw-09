77ab43faa51e736f8e3146129fcd0ae85b3f3219
ZhyvotovskyiSerhii <86567407+ZhyvotovskyiSerhii@users.noreply.github.com>
2021-09-06 21:22:56 +0300

    1 is finished

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+const body = document.querySelector("body");
+
+
+let intervalId;
+let delay = 1000;
+
+startBtn.addEventListener("click", (event) => {
+    event.target.disabled = true;
+    stopBtn.disabled = false;
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+        body.style.backgroundColor = color;
+        return color;
+      }, delay)
+});
+// * - Адская копипаста со стека 💩 - нет, не копипаста))))
+stopBtn.disabled = true;
+stopBtn.addEventListener("click", (event) => {
+    event.target.disabled = true;
+    startBtn.disabled = false;
+    return clearInterval(timerId);
+});
