844e056ff0e8a3a418ee0279111f4fc30833ecfc
ZhyvotovskyiSerhii <86567407+ZhyvotovskyiSerhii@users.noreply.github.com>
2021-09-07 19:35:55 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,27 +1,27 @@
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-const startBtn = document.querySelector("button[data-start]");
-const stopBtn = document.querySelector("button[data-stop]");
-const body = document.querySelector("body");
-
+const refs = {
+startBtn : document.querySelector("button[data-start]"),
+stopBtn : document.querySelector("button[data-stop]"),
+body : document.querySelector("body"),
+}
 let timerId;
 let delay = 1000;
 
-startBtn.addEventListener("click", (event) => {
+refs.startBtn.addEventListener("click", (event) => {
     event.target.disabled = true;
-    stopBtn.disabled = false;
+    refs.stopBtn.disabled = false;
     timerId = setInterval(() => {
         const color = getRandomHexColor();
-        body.style.backgroundColor = color;
+        refs.body.style.backgroundColor = color;
         return color;
       }, delay)
 });
 // * - Адская копипаста со стека 💩 - нет, не копипаста))))
-stopBtn.disabled = true;
-stopBtn.addEventListener("click", (event) => {
+refs.stopBtn.disabled = true;
+refs.stopBtn.addEventListener("click", (event) => {
     event.target.disabled = true;
-    startBtn.disabled = false;
+    refs.startBtn.disabled = false;
     return clearInterval(timerId);
 });
